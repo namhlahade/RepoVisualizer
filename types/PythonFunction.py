a3ebde9ee54ef5ae027795bfae8e9bcda0bc26b3
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2025-03-02 22:15:00 -0500

    stuff

--- types/PythonFunction.py
+++
@@ -1,7 +0,0 @@
-class PythonFunction:
-    def __init__(self, name:str, params:list[(type, str)], return_type:type, description:str):
-        self.name: str = name
-        self.params: list[(type, str)] = params
-        self.return_type: type = return_type
-        self.lines:list[str] = []
-        self.description: str = description
