6c744b8b08c09cca588263ca02712ada1b0f5bf2
Ayon Bhowmick <ayb224@lehigh.edu>
2025-03-04 20:12:18 -0500

    d

--- parser/PythonParser.py
+++ parser/PythonParser.py
@@ -1,4 +1,4 @@
-
+import re
 from type.File import File
 from type.PythonClass import PythonClass
 from type.PythonMethod import PythonMethod
@@ -15,11 +15,16 @@ class PythonParser:
 
 
     def _parse_object(self, obj:str) -> str:
+        obj = obj.strip()
+        print("parsing object", obj)
         objs = obj.split(".")
+        if objs[0] in self.objects:
+            self.objects[".".join(objs[1:])] = objs[0]
         while objs[0] in self.objects:
-            objs[0] = self.objects[objs[0]]
+            objs.insert(0, self.objects[objs[0]])
             if objs[0] == ".":
                 break
+        print("objs", objs)
         return ".".join(objs)
 
     def _parse_params(self, line:str) -> list[(str, str)]:
@@ -29,24 +34,33 @@ class PythonParser:
         if start_params != -1 and end_params != -1:
             param_strs = line[start_params + 1:end_params].split(", ")
             for param_str in param_strs:
+                param_str = param_str.strip()
                 if param_str.__contains__("="):
                     param_str, obj = param_str.split("=")
                     obj = self._parse_object(obj)
-                    self.objects[param_str] = obj
-                param = param_str.split(":")
-                if len(param) == 2:
-                    params.append((param[1], param[0]))
+                    print("obj", obj)
                 else:
-                    params.append((param[0], None))
+                    obj = "."
+                if param_str.__contains__(":"):
+                    param, type_ = param_str.split(":")
+                else:
+                    param = param_str
+                    type_ = None
+                param = param.strip()
+                type_ = type_.strip() if type_ is not None else None
+                obj = obj.strip()
+                params.append((param, type_))
+                self.objects[param] = obj
+                print(self.objects)
         return params
 
-    def _parse_method(self, indent:int) -> PythonMethod:
+    def _parse_method(self, current_name:str, indent:int) -> PythonMethod:
         dec_line = self.lines.pop(0)
-        method_end = dec_line.find("(")
-        method_name:str = dec_line[dec_line.find("def ") + 4:method_end]
+        method_name:str = current_name + "." + dec_line[dec_line.find("def ") + 4:dec_line.find("(")]
         print(f"parsing method {method_name}")
         description:str = ""
         params:list[(str, str)] = self._parse_params(dec_line)
+        print("params", params)
 
         method_lines:list[str] = []
         while len(self.lines) > 0:
@@ -58,10 +72,10 @@ class PythonParser:
             self.lines.pop(0)
         return PythonMethod(method_name, description, params, method_lines)
 
-    def _parse_class(self, indent:int) -> PythonClass:
+    def _parse_class(self, current_name:str, indent:int) -> PythonClass:
         dec_line = self.lines.pop(0)
         class_end = min(i for i in (dec_line.find("("), dec_line.find(":")) if i != -1)
-        class_name:str = dec_line[dec_line.find("class ") + 6:class_end]
+        class_name:str = current_name + "." + dec_line[dec_line.find("class ") + 6:class_end]
         print(print(f"parsing class {class_name}"))
         description:str = ""
         params:list[(str, str)] = self._parse_params(dec_line)
@@ -72,11 +86,11 @@ class PythonParser:
             if line.startswith(("    " * (indent + 1), "\n")):
                 class_lines.append(line[(indent + 1) * 4:])
                 if line.__contains__("def "):
-                    # method = self._parse_method(line)
-                    # self.file.add_method(method, indent + 1)
+                    method = self._parse_method(class_name, indent + 1)
+                    self.file.add_method(method)
                     print("method")
                 elif line.__contains__("class "):
-                    class_ = self._parse_class(indent + 1)
+                    class_ = self._parse_class(class_name, indent + 1)
                     self.file.add_class(class_)
 
             else:
@@ -89,7 +103,7 @@ class PythonParser:
         while len(self.lines) > 0:
             line = self.lines[0]
             if line.startswith("class "):
-                class_ = self._parse_class(0)
+                class_ = self._parse_class("", 0)
                 self.file.add_class(class_)
             if line.startswith("from "):
                 source = line[5:line.find(" import")].strip()
